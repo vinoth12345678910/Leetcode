60b04d1b9fdfa2f1779ece3cf4cb4b5a261e5f4e
Vinoth Anand Gani <anandgani85@gmail.com>
2025-07-19 18:57:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (84.43%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def inorderTraversal(self, root):
+        result = []
+
+        def inorder(node):
+            if not node:
+                return
+            inorder(node.left)
+            result.append(node.val)
+            inorder(node.right)
+
+        inorder(root)
+        return result
+
+        
